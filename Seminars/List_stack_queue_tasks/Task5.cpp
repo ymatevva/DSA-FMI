15da14864cbaa5fff4016c440b1f7381b1e407ea
Йоана Матева <107748308+ymatevva@users.noreply.github.com>
2024-11-14 19:07:32 +0200

    Update Task5.cpp

--- Seminars/List_stack_queue_tasks/Task5.cpp
+++ Seminars/List_stack_queue_tasks/Task5.cpp
@@ -1,122 +1,142 @@
-#include <iostream>
-#include <list>
-#include <queue>
+#include<iostream>
+#include<list>
+#include<queue>
 using namespace std;
 
 void merge(queue<int>& q1, queue<int>& q2) {
 
+	if (q2.empty()) {
+		return;
+	}
 	while (!q2.empty()) {
 		q1.push(q2.front());
 		q2.pop();
 	}
 }
 
-void makeEqual(queue<int>& q1, queue<int>& q2) {
 
-	if (q1.size() > q2.size()) {
+bool equalize(queue<int>& q1, queue<int>& q2) {
+
+	if (q2.empty()) {
+		return false;
+	}
+
+	if (q1.size() > q2.size() + 1) {
+
 		while (q1.size() > q2.size() + 1) {
 			q2.push(q1.front());
 			q1.pop();
 		}
+		return true;
 	}
-	else {
+	else if (q2.size() > q1.size() + 1) {
+
 		while (q2.size() > q1.size() + 1) {
 			q1.push(q2.front());
 			q2.pop();
 		}
+		return true;
 	}
+	return false;
 }
 
-void modifyList(list<queue<int>>& myList) {
+void transform(list<queue<int>>& myList) {
 
-	if (myList.empty() || myList.size() == 1) {
+	if (!myList.size()) {
 		return;
 	}
 
-	auto iter = myList.begin();
+	auto end = myList.end();
+	auto next = myList.begin();
 
-	while (iter != myList.end()) {
+	while (true) {
 
-		auto next = iter;
-		++next;
-
-		if (next == myList.end())
+		auto curr = next++;
+		if (next == end) {
 			break;
-
-		if (iter->size() == next->size()) {
-			merge(*iter, *next);
+		}
+		if ((*curr).size() == (*(next)).size()) {
+			merge(*curr, *(next));
 			myList.erase(next);
-			continue;
+			next = curr;
 		}
 		else {
-			++iter;
-			if (++next == myList.end())
-				break;
 
-			if (!(iter->size() == next->size())) {
-				{
-					makeEqual(*iter, *next);
-					++iter;
+			if (equalize(*curr, *(next))) {
+
+				++next;
+				if (next == end) {
+					break;
 				}
-
 			}
 		}
+
 	}
 }
-
-
-
-
-void print(list<queue<int>>& l) {
-
-	for (auto it = l.begin(); it != l.end(); it++)
+void printQueue(queue<int>& myQ) {
+	for (size_t i = 0; i < myQ.size(); i++)
 	{
-		queue<int>tmp = *it;
-		while (!tmp.empty()) {
-			std::cout << tmp.front() << " ";
-			tmp.pop();
-		}
-		std::cout << endl;
+		int cuur = myQ.front();
+		std::cout << cuur << " ";
+		myQ.pop();
+		myQ.push(cuur);
+	}
+	std::cout << std::endl;
+}
+
+void printList(list<queue<int>>& myL) {
+	auto beg = myL.begin();
+	auto end = myL.end();
+	for (auto it = beg; it != end; it++)
+	{
+		printQueue(*it);
 	}
 }
-	int main() {
+int main() {
 
-		list<queue<int>> l;
+	list<queue<int>> myList;
 
-		queue<int> q1;
-		q1.push(2);
-		q1.push(4);
-		q1.push(6);
-		queue<int> q2;
-		q2.push(8);
-		q2.push(0);
-		q2.push(7);
-		queue<int> q3;
-		q3.push(5);
-		q3.push(7);
-		q3.push(9);
-		q3.push(23);
-		q3.push(22);
-		queue<int> q4;
-		q4.push(9);
-		q4.push(1);
-		queue<int> q5;
-		q5.push(2);
-		queue<int> q6;
-		q6.push(1);
-		queue<int> q7;
-		q7.push(33);
-		q7.push(44);
+	queue<int> q1;
+	q1.push(2);
+	q1.push(4);
+	q1.push(6);
 
-		l.push_back(q1);
-		l.push_back(q2);
-		l.push_back(q3);
-		l.push_back(q4);
-		l.push_back(q5);
-		l.push_back(q6);
-		l.push_back(q7);
+	queue<int> q2;
+	q2.push(8);
+	q2.push(0);
+	q2.push(7);
 
-		modifyList(l);
-		print(l);
-		return 0;
-	}
+	queue<int>q3;
+	q3.push(5);
+	q3.push(7);
+	q3.push(9);
+	q3.push(23);
+	q3.push(22);
+
+	queue<int> q4;
+	q4.push(9);
+	q4.push(1);
+
+	queue<int> q5;
+	q5.push(2);
+
+	queue<int> q6;
+	q6.push(1);
+
+	queue<int> q7;
+	q7.push(33);
+	q7.push(44);
+
+	myList.push_back(q1);
+	myList.push_back(q2);
+	myList.push_back(q3);
+	myList.push_back(q4);
+	myList.push_back(q5);
+	myList.push_back(q6);
+	myList.push_back(q7);
+
+	printList(myList);
+	transform(myList);
+	printList(myList);
+	return 0;
+}
