faf05dfe9172b60d2dad5bd0bcccef433b11727d
Йоана Матева <107748308+ymatevva@users.noreply.github.com>
2024-11-25 13:02:34 +0200

    Add files via upload

---
+++ Seminars/Seminar_8/BFS.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+
+struct Node {
+
+	char data;
+	std::vector<Node*>children;
+};
+
+void BFS(const Node* root) {
+
+	if (!root) {
+		return;
+	}
+
+	std::queue<const Node*> myQ;
+	myQ.push(root);
+
+	while (!myQ.empty()) {
+
+		size_t currSize = myQ.size();
+
+		for (size_t i = 0; i < currSize; i++)
+		{
+			std::cout << myQ.front()->data << " ";
+
+			for (size_t i = 0; i < myQ.front()->children.size(); i++)
+			{
+				myQ.push(myQ.front()->children[i]);
+			}
+			myQ.pop();
+		}
+		std::cout << std::endl;
+	
+	}
+}
+
+void free(Node* root) {
+	if (!root)
+	{
+		return;
+	}
+
+	for (size_t i = 0; i < root->children.size(); i++)
+	{
+		delete root->children[i];
+	}
+	delete root;
+}
+int main() {
+
+	Node* root = new Node{ 'a' };
+
+	root->children.push_back(new Node{ 'b' });
+	root->children.push_back(new Node{ 'c' });
+	root->children.push_back(new Node{ 'd' });
+	root->children.at(0)->children.push_back(new Node{ 'x' });
+	root->children.at(0)->children.push_back(new Node{ 'y' });
+
+	BFS(root);
+
+	return 0;
+}
